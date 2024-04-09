24084e62cb91e0fdd6140a805ccf592da9c61b61
mamzii <mziyaee7@gmail.com>
2024-04-09 15:26:29 +0430

    new change in date 21.1.1403

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 a = 2
 b = 4
 import pandas
-<<<<<<< HEAD
+#<<<<<<< HEAD
 c = 1222
-=======
+#=======
 variable_added_hellow_world_version = 12
->>>>>>> hellow-world-image
+#>>>>>>> hellow-world-image
+new_test = 32
